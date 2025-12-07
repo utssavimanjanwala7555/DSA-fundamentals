f0ca829a06c1860511c580a43e07574ffa920aeb
Utssavi Manjanwala <utssavimanjanwala@gmail.com>
2025-12-07 17:18:33 +0530

    Add files via upload

---
+++ SelectionSorting.java
@@ -0,0 +1,36 @@
+public class SelectionSorting 
+{
+    public static void printArray(int arr[])
+    {
+        for(int i = 0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+
+
+
+    public static void main(String [] args)
+    {
+        int arr[] = {6,2,4,7,3};
+        for(int i=0; i<arr.length-1; i++)
+        {
+            int smallest = i;
+            for(int j = i+1; j < arr.length; j++)
+            {
+                if(arr[smallest] > arr[j])
+                {
+                    smallest = j;
+                }
+            }
+
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+
+        }
+        printArray(arr);
+    }
+}
