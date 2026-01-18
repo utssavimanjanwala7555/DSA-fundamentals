8a2f3f0a517692b2d9744276ebdb00196b9165ea
Utssavi Manjanwala <utssavimanjanwala@gmail.com>
2026-01-18 19:00:18 +0530

    commit

--- prac1.java
+++ prac1.java
@@ -1,7 +1,46 @@
+import java.util.Scanner;
+
 public class prac1 
 {
     public static void main(String[] args) 
     {
-        System.out.println("sanvi ");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the size of array:");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+         
+        System.out.println("Select the operation you want to perform:");
+        System.out.println("1) Insert");
+        System.out.println("2) Delete");
+        System.out.println("3) Search");
+        System.out.println("4) Update");
+        System.out.println("5) Display");
+        System.out.println("6) Exit");
+        
+        int choice = sc.nextInt();
+
+        switch(choice)
+        {
+            case 1: 
+            break;
+
+            case 2: 
+            break;
+
+            case 3: 
+            break;
+
+            case 4: 
+            break;
+
+            case 5: 
+            break;
+
+            case 6: 
+            break;
+        }
+         
+
     }    
 }
