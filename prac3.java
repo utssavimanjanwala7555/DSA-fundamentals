466ebee32ce8e8100337488da3e1bb2c881d5d68
Utssavi Manjanwala <utssavimanjanwala@gmail.com>
2026-02-23 00:58:10 +0530

    commit

---
+++ prac3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class prac3 
+{
+    
+        static void hanoi(int n, char src, char aux, char dest)
+        {
+            if (n == 1)
+            {
+                System.out.println("Move disk 1 from " + src + " to " + dest);
+                return;
+            }
+
+            //step1: move n-1 disks from source to auxiliary
+            hanoi(n-1, src, dest, aux);
+               
+            //step2: move n disk from source to destination
+            System.out.println("Move disk " + n + " from " + src + " to " + dest);
+
+            //step3: move n-1 disks from auxiliary to destination
+            hanoi(n-1, aux, src, dest);
+
+        }
+        public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int disks = sc.nextInt();
+
+            hanoi(disks, 'A','B','C');
+
+            sc.close();
+        }
+
+    
+}
