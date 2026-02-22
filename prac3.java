6de38eb6e15dfbbf77a7fb7705a18c1ab89f37b5
Utssavi Manjanwala <utssavimanjanwala@gmail.com>
2026-02-23 01:00:29 +0530

    commit

--- prac3.java
+++ prac3.java
@@ -24,6 +24,8 @@ public class prac3
         public static void main(String args[])
         {
             Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter the number of disks: ");
             int disks = sc.nextInt();
 
             hanoi(disks, 'A','B','C');
